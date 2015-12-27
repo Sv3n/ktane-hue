c2d58a5573515ac5c2d3cfb1c5e508618691c448
Sven Goossens <sgoossens@gmail.com>
2015-12-27 14:16:26 +0100

    command line parser, recovery, restructuring
    Now gets the bridge ip from the command line. Will add lights later.
    Recovers game state from the info in the log. Allows you to switch on the script mid-round.
    Restructuring / refactoring.

--- ktane_hue.py
+++ ktane_hue.py
@@ -3,6 +3,7 @@ File: ktane_hue.py
 Purpose: Control hue lights from ktane
 Author: Sven Goossens
 """
+import argparse
 import datetime
 from enum import Enum, IntEnum
 import json
@@ -20,18 +21,38 @@ SETTINGS = 'ktane_hue.json'
 # A list with the light ids of the lamps the game should control
 COLOR_LAMPS = ["light1", "light2"]
 
-# The ip of the bridge
-BRIDGE = '192.168.0.42'
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='ktane_hue')
+    parser.add_argument('--bridge', dest='bridge_ip', type=str, required=True, help='Set the bridge ip')
+    parser.add_argument('--explode', dest='explode', required=False, default=False, action='store_const', const=True, help='Test the explosion animation')
+    return parser.parse_args()
 
 
 def main():
     setup_logger()
+    args = parse_arguments()
     if not os.path.exists(SETTINGS):
         pass
-    kt = Ktane()
+    kt = Ktane(args.bridge_ip)
 
     # We run from the ktane folder
     lp = KtaneLogParse('logs/ktane.log')
+
+    if args.explode:
+        kt.action(KtaneAction.round_started)
+        kt.action(KtaneAction.explode)
+        # Assumes explode animation < 100 ticks
+        for _ in range(0, 100):
+            kt.tick()
+            time.sleep(0.1)
+        return
+
+    # Parse the log (but don't update lights).
+    # This recovers the state when the script is restarted.
+    kt.fast_forward(True)
+    lp.parse_wrap(kt)
+    kt.fast_forward(False)
 
     """The main event loop"""
     while True:
@@ -40,7 +61,6 @@ def main():
         # (light) animation tick
         kt.tick()
         # Go to sleep. This value determines the duration of each "frame" of the the light animation
-        return
         time.sleep(0.1)
 
 
@@ -72,11 +92,12 @@ class KtaneAction(IntEnum):
 
 class Ktane():
     """ Class that tracks the game state and controls the lights """
-    def __init__(self):
-        # self.b = Bridge(BRIDGE)
-        self.b = MockBridge(BRIDGE)
+    def __init__(self, bridge_ip):
+        self.b = Bridge(bridge_ip)
+        self.mb = MockBridge(bridge_ip)
         self.b.connect()
-
+        self.tmpb = None
+        self.fast_forward_on = False
         self.state = KtaneState.in_menu
 
         self.pulse = 0
@@ -90,9 +111,22 @@ class Ktane():
                 self.color_lamps.append(l)
             else:
                 l.brightness = 40
+        logger.info("Initialized ktane_hue")
 
     def game_active(self):
-        return self.state == KtaneState.in_game or self.state == KtaneState.in_game_quick
+        return (self.state == KtaneState.in_game or self.state == KtaneState.in_game_quick)
+
+    def fast_forward(self, ff):
+        if ff and not self.fast_forward_on:
+            logger.info("Fast forwarding enabled")
+            self.fast_forward_on = True
+            self.tmpb = self.b
+            self.b = self.mb
+        else:
+            logger.info("Fast forward disabled")
+            self.fast_forward_on = False
+            self.b = self.tmpb
+            self.tmpb = None
 
     def tick(self):
         """The function called by our main event loop"""
@@ -104,7 +138,7 @@ class Ktane():
     def explode(self):
         """Explosion animation. Once done, it moves to the post_portem state"""
         if self.pulse == 1:
-            self.normal_transitions()
+            self.quarter_transitions()
 
         for lamp in self.color_lamps:
             if self.pulse == 0:
@@ -114,11 +148,13 @@ class Ktane():
                 lamp.brightness = 251
                 self.color_green(lamp)
             elif self.pulse == 4:
-                lamp.brightness = 10
+                self.normal_transitions()
+                lamp.brightness = 57
                 self.color_red(lamp)
 
         self.pulse += 1
-        if self.pulse == 50:
+        if self.pulse == 70:
+            self.normal_transitions()
             self.state = KtaneState.post_mortem
             self.pulse = 0
             self.post_mortem()
@@ -135,10 +171,10 @@ class Ktane():
 
         for lamp in self.color_lamps:
             if self.pulse == 0:
-                if self.state == KtaneState.in_game_quick:
+                if self.state == KtaneState.in_game_quick or self.strikes >= 2:
                     self.color_mild_red(lamp)
                 else:
-                    self.color_mild_orange(lamp)
+                    self.color_mild_pink(lamp)
 
             elif self.pulse == 24 / div:
                 self.color_orange(lamp)
@@ -150,7 +186,7 @@ class Ktane():
     def menu_mode(self, lamp):
         """Lamp settings during the main menu / bomb selection"""
         lamp.on = True
-        lamp.brightness = 200
+        lamp.brightness = 251
         self.color_warm_white(lamp)
 
     def post_mortem(self):
@@ -158,8 +194,8 @@ class Ktane():
         logger.debug("post-mortem mode")
         for lamp in self.color_lamps:
             lamp.on = True
-            lamp.brightness = 200
-            self.color_cool_white(lamp)
+            lamp.brightness = 251
+            self.color_coolest_white(lamp)
 
     def quick_transitions(self):
         """Set lamps to quickest transition speed (fastest color change)"""
@@ -180,12 +216,13 @@ class Ktane():
             lamp.transitiontime = t
 
     def action(self, action):
+        logger.info("From state {state}".format(state=self.state))
         if action == KtaneAction.round_started:
             if not self.game_active():
                 self.start_round()
 
         if action == KtaneAction.round_ended:
-            if self.state != KtaneState.exploding:
+            if self.state != KtaneState.exploding or self.fast_forward_on:
                 self.stop_round()
 
         if action == KtaneAction.explode:
@@ -218,6 +255,9 @@ class Ktane():
         if action == KtaneAction.one_minute_left:
             self.state = KtaneState.in_game_quick
 
+        logger.info("To state {state}".format(state=self.state))
+
+
     def start_round(self):
         """Reset state, start round"""
         logger.debug("start_round()")
@@ -245,8 +285,11 @@ class Ktane():
     def color_cool_white(self, l):
         self.color_set(35535, 200, l)
 
+    def color_coolest_white(self, l):
+        self.color_set(35535, 254, l)
+
     def color_warm_white(self, l):
-        self.color_set(30535, 0, l)
+        self.color_set(15460, 113, l)
 
     def color_red(self, l):
         self.color_set(65535, 254, l)
@@ -257,6 +300,9 @@ class Ktane():
     def color_magenta(self, l):
         self.color_set(55535, 254, l)
 
+    def color_mild_pink(self, l):
+        self.color_set(62159, 83, l)
+
     def color_blue(self, l):
         self.color_set(47125, 200, l)
 
@@ -264,7 +310,7 @@ class Ktane():
         self.color_set(13535, 254, l)
 
     def color_mild_orange(self, l):
-        self.color_set(13535, 100, l)
+        self.color_set(17535, 200, l)
 
     def color_green(self, l):
         self.color_set(25650, 254, l)
@@ -293,9 +339,13 @@ class KtaneLogParse:
 
     def parse_log(self, lines, kt):
         """Parse log (list of lines). Pass useful state updates to kt."""
-        for line in lines:
-            if '[State]' in line or '[Bomb]' in line or '[PostGameState]' in line or '[Assets.Scripts.Pacing.PaceMaker]' in line:
-                logger.info('--------> {line}'.format(line=line))
+        for line in lines[-400:-1]:
+            if '[State]' in line or \
+               '[Bomb]' in line or \
+               '[PostGameState]' in line or \
+               '[Assets.Scripts.Pacing.PaceMaker]' in line or \
+               '[Assets.Scripts.DossierMenu.MenuPage]' in line:
+                # logger.info('--------> {line}'.format(line=line))
                 # DEBUG 2015-12-24 18:57:49,884 [Assets.Scripts.Pacing.PaceMaker] Round start! Mission: The First Bomb Pacing Enabled: False
 
                 r = r"[ ]*(?P<log_type>[A-Z]+) (?P<start_time>[^\[]*) \[(?P<component>[^\]]*)\] (?P<state_info>.*)"
@@ -306,9 +356,9 @@ class KtaneLogParse:
                 now = datetime.datetime.now()
 
                 action = self.parse_action(res['state_info'], res['component'])
-                logger.info(action)
 
-                if abs(t - now) < datetime.timedelta(seconds=0.3) or True:
+                if abs(t - now) < datetime.timedelta(seconds=0.2) or kt.fast_forward_on:
+                    logger.info(action)
                     # A recent change: pass to kt
                     # logger.info("State changed to {state}".format(state=res['state_info']))
                     kt.action(action)
@@ -327,7 +377,7 @@ class KtaneLogParse:
         if state_info == "A winner is you!!":
             return KtaneAction.win
 
-        if state_info == 'Results screen bomb binder dismissed (continue). Restarting...':
+        if state_info == 'Results screen bomb binder dismissed (continue). Restarting...' or state_info == 'ReturnToSetupRoom':
             return KtaneAction.result_screen_dismissed_to_menu
 
         if state_info == 'Results screen bomb binder dismissed (retry). Retrying same mission...':
